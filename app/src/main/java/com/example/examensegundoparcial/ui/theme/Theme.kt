59bb2b64f6034ccaa81e6658562ee5753d2ee4e2
Diego Garcia <dg21082004@gmail.com>
2026-05-14 18:36:17 -0600

    Agregue colores, tipografia y tema

--- app/src/main/java/com/example/examensegundoparcial/ui/theme/Theme.kt
+++ app/src/main/java/com/example/examensegundoparcial/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.examensegundoparcial.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -18,26 +17,21 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = PrimaryPurple,
     secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    tertiary = Pink40,
+    background = BackgroundLight,
+    surface = SurfaceWhite,
+    onPrimary = SurfaceWhite,
+    onSecondary = SurfaceWhite,
+    onBackground = TextPrimary,
+    onSurface = TextPrimary,
 )
 
 @Composable
 fun ExamenSegundoParcialTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -45,7 +39,6 @@ fun ExamenSegundoParcialTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
